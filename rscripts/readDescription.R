b34d058080e0c490a736c66f7ff1a71a99cfee49
Matt Pocernich <Matt_Pocernich@hotmail.com>
2014-02-25 17:17:04 -0700

    more progress

---
+++ rscripts/readDescription.R
@@ -0,0 +1,161 @@
+
+
+setwd("originalData/algae/EFR Phytoplankton Data/")
+
+#### batch 8
+
+##
+
+txt <- "Sample Results"
+id <- grepl(txt, OUT$sheet)
+
+
+OUTsub2 <- OUT[id  & !OUT$processed, ]
+
+OUT$processed[OUT$full_file_name %in% OUTsub2$full_file_name] <- TRUE
+
+shortList <- OUT[id, ]
+
+batch4 <- readSelectSheets(shortList)
+
+
+write.table(batch4, "../../../output/step1/batch4.csv", sep = ",",row.names=FALSE)
+
+
+txt <- "Sample.Description; Lab; PREP; Method; UNITS; Result; LOD; Reporting.Limit; Qualifiers; Analyte; Sample"
+id <- grepl(txt, OUT$sheetNames)
+
+### files to explicitly skip first go around.
+id2 <- grepl("graph", OUT$full_file_name,ignore.case=TRUE)
+id3 <- grepl("organized", OUT$full_file_name,ignore.case=TRUE)
+id4 <- grepl("Data Q&A", OUT$full_file_name,ignore.case=TRUE) #### do not include because all files are parsed, split up and contain figures.
+
+
+
+shortListA <- OUT[id & !(id2 | id3| id4), ]
+
+OUT$batch[id & !(id2 | id3)] <- "batch8"
+
+### break into groups based on ncol
+shortList <- subset(shortListA, ncol == 11)
+dimCheck <- 0
+
+for(i in 1:nrow(shortList)){
+  err <-    try( wb     <- loadWorkbook(shortList$full_file_name[i]) )
+  if(class(err) == "try-error") next  
+  
+  temp <- readWorksheet(wb, shortList$sheet[i])
+  if(i == 1){
+    batch8 <- temp
+  }
+  temp$sheet_id <- shortList$sheet_id[i]
+  xlcFreeMemory()
+  print(dim(temp))
+  dimCheck <- dimCheck + nrow(temp)
+  batch8 <- merge(batch8, temp, all = TRUE)
+  
+}
+
+batch8_ncol11 <- batch8
+
+shortList <- subset(shortListA, ncol == 14)
+if(FALSE){
+dimCheck <- 0
+
+for(i in 1:nrow(shortList)){
+  err <-    try( wb     <- loadWorkbook(shortList$full_file_name[i]) )
+  if(class(err) == "try-error") next  
+  
+  temp <- readWorksheet(wb, shortList$sheet[i])
+  if(i == 1){
+    batch8 <- temp
+  }
+  temp$sheet_id <- shortList$sheet_id[i]
+  xlcFreeMemory()
+  print(dim(temp))
+  print(tail(temp))
+  dimCheck <- dimCheck + nrow(temp)
+  batch8 <- merge(batch8, temp, all = TRUE)
+  
+}
+}
+
+
+batch8_ncol14 <- batch8
+
+shortList <- subset(shortListA, ncol == 15)
+
+dimCheck <- 0
+
+for(i in 1:nrow(shortList)){
+  err <-    try( wb     <- loadWorkbook(shortList$full_file_name[i]) )
+  if(class(err) == "try-error") next  
+  
+  temp <- readWorksheet(wb, shortList$sheet[i])
+  if(i == 1){
+    batch8 <- temp
+  }
+  temp$sheet_id <- shortList$sheet_id[i]
+  xlcFreeMemory()
+  print(dim(temp))
+  dimCheck <- dimCheck + nrow(temp)
+  batch8 <- merge(batch8, temp, all = TRUE)
+  
+}
+
+sum(batch8$Analyte != batch8$Analyte.1 )
+sum(batch8$Result != batch8$Result.1 )
+sum(batch8$Sample.Description != batch8$Sample.Description.1 )
+
+batch8_ncol15 <- batch8[, !names(batch8)%in% c("Col12", "Analyte.1", "Sample.Description.1", "Result.1"  )]
+
+#### merge
+
+all <- merge(batch8_ncol11, batch8_ncol15, all = TRUE)
+setwd(homeDir)
+
+id <- nchar(all$Sample.Description) == 24
+fix_samp_desciption <- all[!id, ]
+rest <- all[id, ]
+
+rest <- cbind(rest, result_convert(rest$Result) )
+xx <- split_sampleID(rest$Sample.Description)
+xx <- cbind(rest, xx)
+
+wq_dat <- data.frame(location = xx$station,
+                     sample_date  = xx$date ,
+                     sample_time  = xx$time,
+                     sample_depth = xx$depth_ft ,
+                     lrl_tag_num  = NA,     
+                     analyte  = xx$Analyte,
+                     analyte_code = NA,
+                     result = xx$Result,
+                     units  = xx$UNITS,
+                     qualifiers =xx$Qualifiers , 
+                     detect_limit  = xx$LOD , 
+                     report_limit = xx$Reporting.Limit, 
+                     prep_method  =NA,     
+                     test_method  = NA,    
+                     df = NA,           
+                     lab_id = NA,           
+                     lab_sample_number = NA,
+                     analysis_date = NA,     
+                     imported  = NA,         
+                     sheet_id  = xx$sheet_id,         
+                     original = xx$original,        
+                     qual1  = xx$qual1,
+                     qual2 = xx$qual2,
+                     result_num2 = xx$result_num2,
+                     result_num = xx$result_num,       
+                     ID = xx$ID )
+
+wq_dat<- wq_dat_EFRWQ <- wq_datK <- factor_2_character(wq_dat )
+
+
+setwd(homeDir)
+
+
+if(WRITE){
+  write.table(wq_dat, "processed_data/water_quality.csv", sep = ",", row.names=FALSE, col.names=FALSE, append = TRUE)                    
+            
+}
---
+++ rscripts/readRawData.R
@@ -0,0 +1,64 @@
+setwd("originalData/algae/EFR Phytoplankton Data/")
+
+
+id <- grepl("92Rawdat.xlsx", OUT$file)
+id2 <- grepl("summary", OUT$sheet)
+
+### files to explicitly skip first go around.
+
+OUTsub2 <- OUT[id  & !id2, ]
+OUT$skip[id & id2] <- "Summary Table"
+
+OUT$processed[id] <- TRUE
+
+shortList <- OUTsub2
+#######
+
+i <- 1
+err <-    try( wb     <- loadWorkbook(shortList$full_file_name[i]) )
+if(class(err) == "try-error") print( "File Missing") 
+temp <- NULL
+
+
+for( i in 1:18 ){
+  temp1 <- readWorksheet(wb, sheet = shortList$sheet[i], startRow=6, header = FALSE)
+  temp1$sheet_id <- shortList$sheet_id[i]
+  temp <- rbind(temp, temp1)
+  
+}
+
+### mannually fix last sheet
+
+temp1 <- readWorksheet(wb, sheet = shortList$sheet[19], startRow=6, header = FALSE)
+
+OUT$skip[OUT$full_file_name == shortList$full_file_name[19]] <- "Inconsistent first column"
+
+hdr <- readWorksheet(wb, sheet = shortList$sheet[i], startRow=3, endRow=3, header = FALSE)
+names(temp) <- c(unlist(hdr ), "sheet_id")
+AAA <- temp
+AAA$sample_id <- paste( temp$Station, temp$Collection, "9999", formatC(temp$Depth, flag = "0", width = 3  ), sep = "")
+
+## drop poor format and inconsistent first column.
+id <- nchar(AAA$sample_id) != 24
+
+AAA <- AAA[id, ]
+
+algae <- data.frame(ID = AAA$sample_id,
+                    lake = substr(AAA$sample_id, 2,4),
+                    station = substr(AAA$sample_id, 5, 9),
+                    depth_ft = substr(AAA$sample_id, 22, 24),
+                    date = substr(AAA$sample_id, start=10, stop=17),
+                    taxa = AAA$Taxon,
+                    cell_per_l = AAA$Density,
+                    BV.um3.L = AAA[, 14],  ## how can I be certain about these units?
+                    class = NA,
+                    hab = FALSE,
+                    sheet_id = AAA$sheet_id )
+
+
+setwd(homeDir)
+
+if(WRITE){
+  write.table(algae, "processed_data/algae.csv", row.names=FALSE, sep = ",")      
+  
+}  
