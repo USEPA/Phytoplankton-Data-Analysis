312c7fe5062b81500ea42e406c1be3935d9ed0fe
Jake Beaulieu <beaulieu.jake@epa.gov>
2014-11-12 17:00:07 -0500

    Updated materials related to STORET data processing.

--- rscripts/PreviewProcessedDataJake.R
+++ rscripts/PreviewProcessedDataJake.R
@@ -464,6 +464,136 @@ algae[algae$lake == 'grr', 'sheet_id']
   ggplot(date.yr.lk.chem, aes(lake, total)) + geom_bar() +
     ylab("Total number of sampling dates per lake")   
 
+
+# Result.Value.as.Text
+# Clean up character values and replace "Not Detected" with dl for analytes of interest.
+  storet$Result.Value.as.Text <- gsub("^\\s+|\\s+$", "", storet$Result.Value.as.Text)  # strip leading and trailing spaces
+  storet$Result.Value.as.Text <- gsub(",", "", storet$Result.Value.as.Text)  # strip any commas
+  foo <- is.na(as.numeric(storet$Result.Value.as.Text))  # Index for non-numeric values
+  unique(storet$Result.Value.as.Text[foo])  # All non-numeric values.  Not Detected and 0..1
+  storet[storet$Result.Value.as.Text == "0..1", "Result.Value.as.Text"] = "0.1"
+  sum(foo)  # 9592 instances of "Not Detected".  Should provide dl for analytes I care about
+
+# List of analytes I care about
+  analyte.list <- c("Ammonia-nitrogen", "Copper", "Inorganic nitrogen (nitrate and nitrite)",
+                    "Iron", "Kjeldahl nitrogen", "Manganese", "Molybdenum", "Nitrogen", 
+                    "Nitrogen, mixed forms (NH3), (NH4), organic, (NO2) and (NO3)",
+                    "Phosphorus", "Silica", "Sodium", "Total solids", "Total suspended solids")
+# First, define the "Units" as mg/l for all instances of "Not Detected".
+  for (i in 1:length(analyte.list)){
+    storet[storet$Characteristic.Name == analyte.list[i] &  # For each analyte I care about
+             storet$Result.Value.as.Text == "Not Detected",  # Find censored observations
+           "Units"] = "mg/l"  # Change Units from NA to "mg/l"
+  }
+# Second, create "qual1" field for "<" or ">" for censored observations.  
+# Adopted this nomenclature from water chem file Matt pulled together.
+  storet$qual1 <- ifelse(storet$Characteristic.Name %in% analyte.list &
+                       storet$Result.Value.as.Text == "Not Detected",
+                     "<", NA)  # All censored values are below detection limit
+
+# Now, replace "Not Detected" with detection limit for all analytes of interest.
+# Any Ammonia-nitrogen "Not Detected"
+  storet[storet$Characteristic.Name == "Ammonia-nitrogen" & foo, # APHA~4500-NH3(D) mdl=0.03mg/L
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]
+  storet[storet$Characteristic.Name == "Ammonia-nitrogen" &  # Replace "Not Detected" with 0.03
+      storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.03"
+
+
+# Any Copper "Not Detected"
+  storet[storet$Characteristic.Name == "Copper" & foo, # USEPA~200.8 mdl=0.004mg/L
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  
+  storet[storet$Characteristic.Name == "Copper" &  # Replace "Not Detected" with 0.004
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.004"
+
+
+# Any Inorganic nitrogen (nitrate and nitrite) "Not Detected"
+  storet[storet$Characteristic.Name == "Inorganic nitrogen (nitrate and nitrite)" & foo, 
+         c("Result.Value.as.Text", "Analytical.Proc.ID")] 
+  # APHA~4500-NO3(H) mdl=0.01mg/L; USEPA~300(A) mdl=0.01; 
+  storet[storet$Characteristic.Name == "Inorganic nitrogen (nitrate and nitrite)" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.01"
+
+
+# Any Iron "Not Detected"
+  storet[storet$Characteristic.Name == "Iron" & foo, 
+         c("Result.Value.as.Text", "Analytical.Proc.ID")] 
+  # Both referenced methods are for ICP-MS, but neither method actually addresses Iron.
+  # Using Fe mdl reported for ICP in APHA
+  # AUSCOEKY~SW6020A reported as ICP-MS mdl=0.013mg/L; USEPA~200.8 mdl=0.013mg/L; 
+  storet[storet$Characteristic.Name == "Iron" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.013"
+
+
+# Any Kjeldahl nitrogen "Not Detected"
+  storet[storet$Characteristic.Name == "Kjeldahl nitrogen" & foo, # USEPA~351.2 mdl=0.1
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]
+  storet[storet$Characteristic.Name == "Kjeldahl nitrogen" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.1"
+
+
+# Any Manganese "Not Detected"
+  storet[storet$Characteristic.Name == "Manganese" & foo, # USEPA~200.8 mdl=0.007mg/L
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]
+  storet[storet$Characteristic.Name == "Manganese" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.007"
+
+
+# Any Molybdenum "Not Detected"
+  storet[storet$Characteristic.Name == "Molybdenum" & foo, # None
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]
+# Any Nitrogen "Not Detected"
+  storet[storet$Characteristic.Name == "Nitrogen" & foo, # None
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]
+
+# Any Nitrogen, mixed forms (NH3), (NH4), organic, (NO2) and (NO3) "Not Detected"
+  storet[storet$Characteristic.Name == "Nitrogen, mixed forms (NH3), (NH4), organic, (NO2) and (NO3)" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  # USEPA 9056 mdl not reported.  Assume mdl=0.01mg/L
+  storet[storet$Characteristic.Name == "Nitrogen, mixed forms (NH3), (NH4), organic, (NO2) and (NO3)" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.01"
+
+# Any Phosphorus "Not Detected"
+  storet[storet$Characteristic.Name == "Phosphorus" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  # APHA~4500-P-F mdl=0.005mg/L
+  storet[storet$Characteristic.Name == "Phosphorus" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.005"
+
+
+# Any Silica "Not Detected"
+  storet[storet$Characteristic.Name == "Silica" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  # None
+# Any Sodium "Not Detected"
+  storet[storet$Characteristic.Name == "Sodium" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  # None
+
+# Any Total solids "Not Detected"
+  storet[storet$Characteristic.Name == "Total solids" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")]  
+    # APHA~2540-B no mdl reported.  set = to 1/2 lowest reported value in dataset = 0.5
+  storet[storet$Characteristic.Name == "Total solids" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = "0.5"
+
+# Any Total suspended solids "Not Detected"
+  storet[storet$Characteristic.Name == "Total suspended solids" & foo,
+         c("Result.Value.as.Text", "Analytical.Proc.ID")] 
+  # APHA~2540-D no mdl reported.  set = to 1/2 lowest reported value in dataset
+  storet[storet$Characteristic.Name == "Total suspended solids" &  
+           storet$Result.Value.as.Text == "Not Detected",
+         "Result.Value.as.Text"] = min(storet[storet$Characteristic.Name == "Total suspended solids",
+                                              "Result.Value.as.Text"])
+
+# DEAL WITH UNITS.  CONVERT EVERYTHING TO mg/l
+unique(storet$Units)
+
+
 # Analyte names
 # Pull out all unique combination of "Characteristic.Name" and "Analytical.Proc.ID"
   name.method <- data.frame(stringsAsFactors=FALSE,
@@ -477,18 +607,28 @@ algae[algae$lake == 'grr', 'sheet_id']
               row.names=FALSE)
   # Inspect table in Excel.  Reference appropriate Methods and provide appropriate analyte name
 
- 
+  # Units: 
+    # Ammonia-nitrogen: mg/kg --mg/L
+    # Copper: mg/L -- ug/l
+    # Inorganic nitrogen (nitrate and nitrite): mg/kg --mg/L
+    # Iron: mg/L -- ug/L
+    # Kjeldahl nitrogen: mg/kg --mg/L
+    # Manganese: mg/l -- ug/l
+    # Molybdenum
+    # Phosphorus: ug/l - mg/l:  mg/kg -- mg/l
+    # Silica: mg
+    
+    
+  # dl
+    # Nitrogen, mixed forms (NH3), (NH4), organic, (NO2) and (NO3) via USEPA~9056
+    # has a few "Not Detected".  SOP does not provide dl.  Suggest "10 ug/l" based on experience.
 
-  unique(storet$Characteristic.Name)[grepl("nitrogen", unique(storet$Characteristic.Name))]
-  unique(storet$Characteristic.Name)[grepl("Phosphorus", unique(storet$Characteristic.Name))]
-  unique(storet$Characteristic.Name)[grepl("phosphorus", unique(storet$Characteristic.Name))]
-  unique(storet[storet$Characteristic.Name == "Phosphorus", c("Characteristic.Name", "Sample.Fraction")])
-  unique(storet[storet$Characteristic.Name == "Iron", c("Characteristic.Name", "Sample.Fraction")]) 
-  unique(storet[storet$Characteristic.Name == "Molybdenum", c("Characteristic.Name", "Sample.Fraction")])   
-  unique(storet[storet$Characteristic.Name == "Copper", c("Characteristic.Name", "Sample.Fraction")])  
-  unique(storet[storet$Characteristic.Name == "Manganese", c("Characteristic.Name", "Sample.Fraction")]) 
-  unique(storet[storet$Characteristic.Name == "Sodium", c("Characteristic.Name", "Sample.Fraction")])
-  unique(storet[storet$Characteristic.Name == "Silica", c("Characteristic.Name", "Sample.Fraction")])
+
+foo <- storet[storet$Characteristic.Name == "Silica", c("Units", "Result.Value.as.Text")]
+unique(foo)
+str(foo)
+table(foo)
+unique(storet$Analytical.Proc.ID)
 
 # Water Temperature
   # Prepare data for plotting
